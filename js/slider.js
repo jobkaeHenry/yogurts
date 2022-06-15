545191d9bd0b46bdf60aa14e31f7a32afc8b993a
jobkaehenry <1zzangjun@gmail.com>
2022-06-16 00:59:58 +0900

    mobile

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('#slider_banner').slick({
+    slidesToShow:1,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true
+  });
+})
